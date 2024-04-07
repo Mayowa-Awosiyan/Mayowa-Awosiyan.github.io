1ae83bc4b464961f90686f34453b7d1780cddbfe
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-04-07 14:15:15 -0400

    Email sending

--- email.js
+++ email.js
@@ -1,41 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const sgMail = require("@sendgrid/mail");
-const app = express();
-const port = 3000;
+document.getElementById("regForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-app.use(bodyParser.json());
+  const email = document.getElementById("regEmail").value;
 
-app.post("/send-email", (req, res) => {
-  const { fname, email, availability } = req.body;
-
-  const msg = {
-    to: email, // Change to your recipient
-    from: "surrogateavatar@outlook.com", // Change to your verified sender
-    subject: "Availability Confirmation",
-    text:
-      "Hello " +
-      fname +
-      "! This email is to confirm that you are registered as a Surrogate avatar that is available at " +
-      availability +
-      ". If this time is incorrect please go back to the web page and change your start time",
-    html:
-      "<strong>This email is to confirm that you are registered as a Surrogate avatar that is available at " +
-      availability +
-      ". If this time is incorrect please go back to the web page and change your start time</strong>",
-  };
-
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-      res.json({ message: "Email sent successfully" });
+  if (email) {
+    // Send the form data to the server
+    fetch("/sendConfirmationEmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ error: "Failed to send email" });
-    });
-});
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    console.log("Invalid email");
+  }
 });
