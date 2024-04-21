5f2f2b7fcdba996b8c5708b7291070027d3109ff
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-04-20 22:10:44 -0400

    Login and Logout

--- index.js
+++ index.js
@@ -1,6 +1,84 @@
+//firebase login
+import { app } from "./firebaseconfig.js";
+import {
+  getAuth,
+  signInWithPopup,
+  OAuthProvider,
+  signInWithRedirect,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const auth = getAuth();
+const provider = new OAuthProvider("microsoft.com");
 // Grab the consent button and buttons with the card class
 const consentButton = document.getElementById("consentbtn");
 const cardButtons = document.querySelectorAll(".card");
+const loginbtn = document.getElementById("UoLogin");
+const logoutbtn = document.getElementById("logoutbtn");
+//This should make it so only uottawa associated accounts can use the service
+//Tenant ID can be found in azure by using a Uottawa account
+/*
+provider.setCustomParameters({
+  tenant: tenantID,
+});
+*/
+
+//provider.addScope("calendars.read");
+//provider.addScope("calendars.write");
+
+provider.setCustomParameters({
+  prompt: "consent",
+});
+
+loginbtn.addEventListener("click", function () {
+  console.log("Login in Clicked");
+  /*signInWithPopup(auth, provider)
+    .then((result) => {
+      // User is signed in.
+      // IdP data available in result.additionalUserInfo.profile.
+
+      // Get the OAuth access token and ID Token
+      console.log(result);
+      const credential = OAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+      const idToken = credential.idToken;
+    })
+    .catch((error) => {
+      // Handle error.
+      console.log(error);
+    });
+    */
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // User is signed in.
+      // IdP data available in result.additionalUserInfo.profile.
+      console.log(result, "THAT's ALL FOLKS");
+
+      // Get the OAuth access token and ID Token
+      const credential = OAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+      const idToken = credential.idToken;
+    })
+    .catch((error) => {
+      // Handle error.
+      console.log("SO CLOSE!!");
+      console.log(error);
+    });
+});
+//Logout the current user when the log out button is clicked
+logoutbtn.addEventListener("click", function () {
+  const auth = getAuth();
+  console.log("attempting");
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      console.log("USER LOGGED OUT");
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+});
 
 // Function to enable card buttons
 function enableCardButtons() {
